10b25cf42c7c06280dd9b26d00b38c4cc59e6250
vishnu <vishnurapuru10@gmail.com>
2020-10-06 18:20:12 +0530

    initial commit

---
+++ Design_patterns_programs/src/observer/SpreadSheet.java
@@ -0,0 +1,9 @@
+package observer;
+
+public class SpreadSheet implements Observer {
+
+    @Override
+    public void update() {
+        System.out.println("Spreadsheet got updated");
+    }
+}
---
+++ Design_patterns_programs/src/observer/Subject.java
@@ -0,0 +1,20 @@
+package observer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//Observable
+public class Subject {
+    List<Observer> observers = new ArrayList<>();
+    public void addObserver(Observer observer){
+        observers.add(observer);
+    }
+    public void removeObserver(Observer observer){
+        observers.remove(observer);
+    }
+    public void notifyObserver(){
+        for(Observer observer : observers){
+            observer.update();
+        }
+    }
+}
